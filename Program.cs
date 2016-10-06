3aa9143662c112fc79cbb4047ac68869a2473669
bobanko <mishko.vladimir@gmail.com>
2016-10-06 23:39:06 +0300

    enable debugging

--- Program.cs
+++ Program.cs
@@ -6,33 +6,35 @@ using System.IO;
 
 namespace PowerGrid.API
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                 .UseIISIntegration()//
-                .UseStartup<Program>()
-                .Build();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.CaptureStartupErrors(true)
+				.UseSetting("detailedErrors", "true")
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				 .UseIISIntegration()//
+				.UseStartup<Program>()
+				.Build();
 
-            host.Run();
-        }
+			host.Run();
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
 
-             services.AddSingleton<INoteRepository,NoteRepository>();
-        }
+			services.AddSingleton<INoteRepository, NoteRepository>();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseMvcWithDefaultRoute();
-        }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseMvcWithDefaultRoute();
+		}
 
-    }
+	}
 }
