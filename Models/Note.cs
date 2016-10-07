222cdc28b417210681f045145e243843a08757ca
Igor Gavrilov <igavrilov@inspiresmart.com>
2016-10-08 01:50:20 +0300

    Web api initial commit

--- Models/Note.cs
+++
@@ -1,9 +0,0 @@
-namespace PowerGrid.API.Models
-{
-    public class Note
-    {
-        public string Key {get;set;}
-        public string Subject {get;set;}
-        public string Body {get;set;}
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# PowerGridApi
-api proj for powerGrid
