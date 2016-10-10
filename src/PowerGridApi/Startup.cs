f3c152e539ddb58a123e5c6a4c41e31887e8f440
bobanko <mishko.vladimir@gmail.com>
2016-10-11 02:44:34 +0300

    CORS enabled

--- src/PowerGridApi/Startup.cs
+++ src/PowerGridApi/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,64 +7,66 @@ using PowerGridEngine;
 
 namespace PowerGridApi
 {
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
 
-            //init energo server
-            EnergoServer.Current.Settings.SimpleOrGuidPlayerId = true;
-            ServerContext.InitCurrentContext(EnergoServer.Current, new Logger());
-        }
+			//init energo server
+			EnergoServer.Current.Settings.SimpleOrGuidPlayerId = true;
+			ServerContext.InitCurrentContext(EnergoServer.Current, new Logger());
+		}
 
-        public IConfigurationRoot Configuration { get; }
+		public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Add framework services.
+			services.AddMvc();
 
-            var xmlPath = GetXmlCommentsPath();
-            services.AddSwaggerGen();
-            services.ConfigureSwaggerGen(options =>
-            {
-                options.SingleApiVersion(new Swashbuckle.Swagger.Model.Info
-                {
-                    Version = Controllers.CommonController.Version,
-                    Title = "Power Grid API",
-                    Description = "API for Power Grid Game developed by AgeStone Team",
-                    TermsOfService = "None"
-                });
-                options.IncludeXmlComments(xmlPath);
-                options.DescribeAllEnumsAsStrings();
-            });
+			services.AddCors();
 
-        }
+			var xmlPath = GetXmlCommentsPath();
+			services.AddSwaggerGen();
+			services.ConfigureSwaggerGen(options =>
+			{
+				options.SingleApiVersion(new Swashbuckle.Swagger.Model.Info
+				{
+					Version = Controllers.CommonController.Version,
+					Title = "Power Grid API",
+					Description = "API for Power Grid Game developed by AgeStone Team",
+					TermsOfService = "None"
+				});
+				options.IncludeXmlComments(xmlPath);
+				options.DescribeAllEnumsAsStrings();
+			});
 
-        private string GetXmlCommentsPath()
-        {
-            var app = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application;
-            return System.IO.Path.Combine(app.ApplicationBasePath, "PowerGridApi.xml");
-        }
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+		private string GetXmlCommentsPath()
+		{
+			var app = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application;
+			return System.IO.Path.Combine(app.ApplicationBasePath, "PowerGridApi.xml");
+		}
 
-            app.UseMvc();
-            app.UseSwagger();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
 
-            var ver = Controllers.CommonController.Version;
-            app.UseSwaggerUi("api/help", string.Format("/swagger/{0}/swagger.json", ver));
-        }
-    }
+			app.UseMvc();
+			app.UseSwagger();
+
+			var ver = Controllers.CommonController.Version;
+			app.UseSwaggerUi("api/help", string.Format("/swagger/{0}/swagger.json", ver));
+		}
+	}
 }
