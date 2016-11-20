037720bf308b8561fac6e28ed23a42691559f82c
Mishko Vladimir <mishko.vladimir@gmail.com>
2016-11-21 00:00:19 +0200

    websockets basic impl

--- src/PowerGridApi/Startup.cs
+++ src/PowerGridApi/Startup.cs
@@ -12,33 +12,7 @@ using System.Linq;
 
 namespace PowerGridApi
 {
-    //public class SwaggerParameter : Swashbuckle.Swagger.Model.IParameter
-    //{
-    //    public string Description { get; set; }
-    //    public string In { get; set; }
-    //    public string Name { get; set; }
-    //    public bool Required { get; set; }
-
-    //    public Dictionary<string, object> Extensions { get { return new Dictionary<string, object>(); } }
-    //}
-
-    //public class AddCustomAuthHeaderParameterOperationFilter : Swashbuckle.SwaggerGen.Generator.IOperationFilter
-    //{
-    //    public void Apply(Swashbuckle.Swagger.Model.Operation operation, Swashbuckle.SwaggerGen.Generator.OperationFilterContext context)
-    //    {
-    //        var param = new SwaggerParameter
-    //        {
-    //            Name = "CustomAuth",
-    //            In = "header",
-    //            Description = "access token",
-    //            Required = false,
-    //        };
-    //        if (operation.Parameters != null && operation.OperationId == "ApiLogoutByUserIdPost")
-    //            operation.Parameters.Add(param);
-    //    }
-    //}
-
-    public partial class Startup
+	public partial class Startup
 	{
 		public Startup(IHostingEnvironment env)
 		{
@@ -52,15 +26,15 @@ namespace PowerGridApi
 			//init energo server
 			EnergoServer.Current.Settings.SimpleOrGuidPlayerId = false;
 			ServerContext.InitCurrentContext(EnergoServer.Current, new Logger());
-        }
+		}
 
 		public IConfigurationRoot Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-            // Add service and create Policy with options
-            services.AddCors(options =>
+			// Add service and create Policy with options
+			services.AddCors(options =>
 			{
 				options.AddPolicy("CorsPolicy",
 					builder => builder.AllowAnyOrigin()
@@ -71,33 +45,32 @@ namespace PowerGridApi
 
 			//services.AddCors();
 
-
 			// Add framework services.
 			services.AddMvc();
 
 
-            var responseStatusesLst = (IEnumerable<ResponseType>)Enum.GetValues(typeof(ResponseType));
-            var responseStatuses = string.Join(", ", responseStatusesLst.Select(m => m.ToString()));
+			var responseStatusesLst = (IEnumerable<ResponseType>)Enum.GetValues(typeof(ResponseType));
+			var responseStatuses = string.Join(", ", responseStatusesLst.Select(m => m.ToString()));
 
-            //Configure Swagger - tool for UI Help about API
-            var xmlPath = GetXmlCommentsPath();
+			//Configure Swagger - tool for UI Help about API
+			var xmlPath = GetXmlCommentsPath();
 			services.AddSwaggerGen();
 			services.ConfigureSwaggerGen(options =>
 			{
-                options.SingleApiVersion(new Swashbuckle.Swagger.Model.Info
-                {
-                    Version = Controllers.CommonController.SwaggerVersion,
-                    Title = "Power Grid API",
-                    Description = string.Format("API for Power Grid Game developed by AgeStone Team.<br/> All Possible response statuses: {0}", responseStatuses),
-                    TermsOfService = "None"
+				options.SingleApiVersion(new Swashbuckle.Swagger.Model.Info
+				{
+					Version = Controllers.CommonController.SwaggerVersion,
+					Title = "Power Grid API",
+					Description = string.Format("API for Power Grid Game developed by AgeStone Team.<br/> All Possible response statuses: {0}", responseStatuses),
+					TermsOfService = "None"
 				});
 				options.IncludeXmlComments(xmlPath);
 				options.DescribeAllEnumsAsStrings();
-                //options.OperationFilter<AddCustomAuthHeaderParameterOperationFilter>();
-            });
+				//options.OperationFilter<AddCustomAuthHeaderParameterOperationFilter>();
+			});
 
-            //todo: maybe this is redundant when we enable all origins below
-            services.Configure<MvcOptions>(options =>
+			//todo: maybe this is redundant when we enable all origins below
+			services.Configure<MvcOptions>(options =>
 			{
 				options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigin"));
 			});
@@ -112,17 +85,18 @@ namespace PowerGridApi
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            app.UseStaticFiles();
+		{
+			app.UseStaticFiles();
 
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
 			loggerFactory.AddDebug();
 
 
-            //Configure CORS
+			//Configure CORS
 			app.UseCors("CorsPolicy");
-			//app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
+			app.UseWebSockets();
+			app.Use(ws.WebSocketManager.Listen);
 
 			app.UseMvc(routes =>
 			{
@@ -132,14 +106,14 @@ namespace PowerGridApi
 			});
 
 
-            //Configure Authorization
-            //ConfigureOAuth(app);
+			//Configure Authorization
+			//ConfigureOAuth(app);
 
-            //Configure Swagger - tool for UI Help about API
-            app.UseSwagger();
-            app.UseSwaggerUi("api/help", string.Format("/swagger/{0}/swagger.json", Controllers.CommonController.SwaggerVersion));
-            
+			//Configure Swagger - tool for UI Help about API
+			app.UseSwagger();
+			app.UseSwaggerUi("api/help", string.Format("/swagger/{0}/swagger.json", Controllers.CommonController.SwaggerVersion));
 
-        }
+
+		}
 	}
 }
---
+++ src/PowerGridApi/ws/WebSocketManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace PowerGridApi.ws
+{
+	public static class WebSocketManager
+	{
+
+		static ConcurrentBag<WebSocket> _sockets = new ConcurrentBag<WebSocket>();
+
+		static ArraySegment<Byte> GetBytes(string str)
+		{
+			byte[] bytes = new byte[str.Length * sizeof(char)];
+			System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+			return new ArraySegment<Byte>(bytes);
+		}
+
+		public static async void Broadcast(string message)
+		{
+			//await Task.WhenAll(_sockets.Where(s.State == WebSocketState.Open).Select(s => s.Send());
+
+			var webSocket = _sockets.First();
+
+			await Task.WhenAll(
+				_sockets
+				.Where(s => s.State == WebSocketState.Open)
+				.Select(s => s.SendAsync(GetBytes(message), WebSocketMessageType.Text, true, CancellationToken.None)));
+
+			//await webSocket.SendAsync(GetBytes(message), WebSocketMessageType.Text, true, CancellationToken.None);
+		}
+
+
+
+
+		public static async Task Listen(HttpContext http, Func<Task> next)
+		{
+			//_sockets.Add(webSocket);
+			if (http.WebSockets.IsWebSocketRequest)
+			{
+				var webSocket = await http.WebSockets.AcceptWebSocketAsync();
+
+
+				if (webSocket != null && webSocket.State == WebSocketState.Open)
+				{
+					_sockets.Add(webSocket);
+					//todo: somehow delete closed sockets
+
+					while (webSocket.State == WebSocketState.Open)
+					{
+						var error = String.Empty;
+						try
+						{
+							var buffer = new ArraySegment<Byte>(new Byte[4096]);
+							var received = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+							switch (received.MessageType)
+							{
+								case WebSocketMessageType.Close:
+									OnClose(received.CloseStatus, received.CloseStatusDescription);
+									//todo: somehow delete closed sockets
+									break;
+								case WebSocketMessageType.Text:
+									var message = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count).Trim('\0');
+									OnMessage(message);
+									Broadcast(message);
+									break;
+								case WebSocketMessageType.Binary:
+									OnMessage(buffer.Array);
+									break;
+							}
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex);
+							break;
+						}
+					}
+				}
+				else
+				{
+					// Nothing to do here, pass downstream.  
+					await next();
+				}
+			}
+		}
+
+
+		static void OnClose(WebSocketCloseStatus? status, string desc)
+		{
+			Console.WriteLine(desc);
+		}
+
+		static void OnMessage(string data)
+		{
+			Console.WriteLine(data);
+		}
+
+		static void OnMessage(byte[] data)
+		{
+			Console.WriteLine(data);
+		}
+
+	}
+}
