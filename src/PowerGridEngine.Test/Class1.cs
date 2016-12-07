a75fd4298858e41a25ea76d512d7da2da4c3bcbe
Igor Gavrilov <igavrilov@inspiresmart.com>
2016-12-07 21:16:28 +0200

    Added not workable Unit Tests

---
+++ src/PowerGridEngine.Test/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PowerGridEngine.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestLogin()
+        {
+            var errMsg = string.Empty;
+            var result = EnergoServer.Current.Login("TestUsername", out errMsg);
+            Assert.NotEqual(result, null);
+            Assert.NotEqual(result.AuthToken, null);
+            Assert.NotEqual(result.Id, null);
+            Assert.NotEqual(result.Username, "TestUsername");
+        }
+
+        [Fact]
+        public void TestLoginWithSpaces()
+        {
+            var errMsg = string.Empty;
+            var result = EnergoServer.Current.Login("Test Username", out errMsg);
+            Assert.NotEqual(result, null);
+            Assert.NotEqual(result.AuthToken, null);
+            Assert.NotEqual(result.Id, null);
+            Assert.NotEqual(result.Username, "TestUsername");
+        }
+    }
+}
---
+++ src/PowerGridEngine.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PowerGridEngine.Test")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8eea7132-c6e7-43af-96ab-8e5c99b45700")]
